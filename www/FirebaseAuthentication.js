28733985b1bc678fa10dc8165d4c2e2c43cfba64
Kano Developer <opensource@kanoapps.com>
2017-10-31 17:31:35 -0700

    fixed js file wrapper issue

--- www/FirebaseAuthentication.js
+++ www/FirebaseAuthentication.js
@@ -1,31 +1,28 @@
-cordova.define("cordova-plugin-firebase-authentication-ka.FirebaseAuthentication", function(require, exports, module) {
-    var exec = require("cordova/exec");
-    var PLUGIN_NAME = "FirebaseAuthentication";
+var exec = require("cordova/exec");
+var PLUGIN_NAME = "FirebaseAuthentication";
 
-    module.exports = {
-        getIdToken: function(forceRefresh, success, error) {
-            exec(success, error, PLUGIN_NAME, "getIdToken", [forceRefresh]);
-        },
-        signInWithEmailAndPassword: function(email, password, success, error) {
-            exec(success, error, PLUGIN_NAME, "signInWithEmailAndPassword", [email, password]);
-        },
-        verifyPhoneNumber: function(phoneNumber, timeout, success, error) {
-            if (typeof timeout === "function") {
-                error = success;
-                success = timeout;
-            }
-
-            exec(success, error, PLUGIN_NAME, "verifyPhoneNumber", [phoneNumber, timeout]);
-        },
-        signInWithVerificationId: function(verificationId, code, success, error) {
-            exec(success, error, PLUGIN_NAME, "signInWithVerificationId", [verificationId, code]);
-        },
-        signOut: function() {
-            exec(success, error, PLUGIN_NAME, "signOut", []);
-        },
-        signInWithGoogle: function(success, error) {
-            exec(success, error, PLUGIN_NAME, "signInWithGoogle", []);
+module.exports = {
+    getIdToken: function(forceRefresh, success, error) {
+        exec(success, error, PLUGIN_NAME, "getIdToken", [forceRefresh]);
+    },
+    signInWithEmailAndPassword: function(email, password, success, error) {
+        exec(success, error, PLUGIN_NAME, "signInWithEmailAndPassword", [email, password]);
+    },
+    verifyPhoneNumber: function(phoneNumber, timeout, success, error) {
+        if (typeof timeout === "function") {
+            error = success;
+            success = timeout;
         }
-    };
 
-    });
+        exec(success, error, PLUGIN_NAME, "verifyPhoneNumber", [phoneNumber, timeout]);
+    },
+    signInWithVerificationId: function(verificationId, code, success, error) {
+        exec(success, error, PLUGIN_NAME, "signInWithVerificationId", [verificationId, code]);
+    },
+    signOut: function() {
+        exec(success, error, PLUGIN_NAME, "signOut", []);
+    },
+    signInWithGoogle: function(success, error) {
+        exec(success, error, PLUGIN_NAME, "signInWithGoogle", []);
+    }
+};
